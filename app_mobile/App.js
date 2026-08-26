c7b8961e33b9ecb7f6e4f43bb8487b295947f97d
jcorjr <julio.cesar.junior200052@gmail.com>
2026-08-26 14:48:04 +0000

    Estilização externa

--- app_mobile/App.js
+++ app_mobile/App.js
@@ -1,46 +1,21 @@
 import React from 'react';
-import {Text, View, Image, StyleSheet, ImageBackground} from 'react-native';
+import {Text, View, Image, ImageBackground} from 'react-native';
+import styles from './styles/styles'
 
 export default function App() {
   return ( 
     <ImageBackground
-      source={require('./assets/imagem.jpeg')}
+      source={require('./assets/imagens/Fundo.jpg')}
       style={styles.background}
       resizeMode="cover"
     >
       <View style={styles.container}>
         <Text style={styles.title}> Meu App</Text>
-        <Text style={styles.subtitle}>Bem Vienido!</Text>
-      
+        <Text style={styles.text}>Bem Vienido!</Text>
 
-        <button title="Entrar" onProgress={()=>{}}/> 
+        {/* <button title="Entrar" onProgress={()=>{}}/>  */}
       </View>
     </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-  },
-
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding:20,
-  },
-
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#ffffff',
-    marginBottom: 10
-  },
-
-  subtitle: {
-    fontSize: 18,
-    color: 'ffffff',
-    marginBottom: 20,
-  },
-});
---
+++ app_mobile/styles/styles.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from "react-native"
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "#ffff",
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#222222',
+    },
+    text: {
+        fontSize: 18,
+        color: '#555555'
+    },
+    
+});
+
+export default styles;
